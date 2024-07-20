965c100e173426d8c285a105066a32889f98bade
Richson-Tech <folorunshoahmedolajide@gmail.com>
2024-07-20 10:36:10 +0100

    new commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,9 @@
 import First from "@/components/First";
-import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-     <First/> 
+      <First />
     </main>
   );
 }
